5e628c8b00da0c6e271ef4e9379de1ee45657103
lskq <lskq012025@gmail.com>
2025-04-07 14:01:48 +0200

    Remove unnecessary expressions from HeaderChunk

--- MIDILib/Chunks/HeaderChunk.cs
+++ MIDILib/Chunks/HeaderChunk.cs
@@ -7,9 +7,4 @@ public class HeaderChunk(byte[] bytes) : IChunk
     public int Format => Bytes[8] * 2 + Bytes[9];
     public int Ntrks => Bytes[10] * 2 + Bytes[11];
     public int Division => Bytes[12] * 2 + Bytes[13];
-
-    public string FormatType => (Format == 0) ? "Single Track" :
-                                (Format == 1) ? "Multiple Tracks" :
-                                (Format == 2) ? "Multiple Patterns" : "Unknown";
-    public string DivisionType => (Division > 0x7FFF) ? "SMPTE" : "Ticks per quarter-note";
 }
