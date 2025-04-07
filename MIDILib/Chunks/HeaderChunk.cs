8000ef1be134b37a3927e366cd51b5cfbf1d6608
lskq <lskq012025@gmail.com>
2025-04-07 14:04:59 +0200

    Fix math in HeaderChunk expressions

--- MIDILib/Chunks/HeaderChunk.cs
+++ MIDILib/Chunks/HeaderChunk.cs
@@ -4,7 +4,7 @@ public class HeaderChunk(byte[] bytes) : IChunk
 {
     public byte[] Bytes { get; } = bytes;
 
-    public int Format => Bytes[8] * 2 + Bytes[9];
-    public int Ntrks => Bytes[10] * 2 + Bytes[11];
-    public int Division => Bytes[12] * 2 + Bytes[13];
+    public int Format => (Bytes[8] << 8) + Bytes[9];
+    public int Ntrks => (Bytes[10] << 8) + Bytes[11];
+    public int Division => (Bytes[12] << 8) + Bytes[13];
 }
